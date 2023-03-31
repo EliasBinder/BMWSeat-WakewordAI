4e363deb34663c26cd007639b3ae7a5163f71342
Elias Binder <ebinder@unibz.it>
2023-03-31 16:29:39 +0200

    Add training data for stop, start with runner.py for tensorflow model

---
+++ src/ai/runner.py
@@ -0,0 +1,73 @@
+import os
+import pathlib
+
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+import tensorflow as tf
+
+from tensorflow.keras import layers
+from tensorflow.keras import models
+from IPython import display
+
+# Set the seed value for experiment reproducibility.
+seed = 42
+tf.random.set_seed(seed)
+np.random.seed(seed)
+
+# Import the mini Speech Commands dataset
+
+DATASET_PATH = 'resources/commands'
+data_dir = pathlib.Path(DATASET_PATH)
+
+# Get the commands from the folder names.
+
+commands = np.array(tf.io.gfile.listdir(str(data_dir)))
+commands = commands[(commands != 'README.md') & (commands != '.DS_Store')]
+print('Commands:', commands)
+
+# Load the data
+
+train_ds, val_ds = tf.keras.utils.audio_dataset_from_directory(
+    directory=data_dir,
+    batch_size=64,
+    validation_split=0.2,
+    seed=0,
+    output_sequence_length=16000,
+    subset='both'
+)
+
+label_names = np.array(train_ds.class_names)
+print()
+print("label names:", label_names)
+
+# Drop extra axis from the labels
+
+def squeeze(audio, labels):
+  audio = tf.squeeze(audio, axis=-1)
+  return audio, labels
+
+
+train_ds = train_ds.map(squeeze, tf.data.AUTOTUNE)
+val_ds = val_ds.map(squeeze, tf.data.AUTOTUNE)
+
+# Visualize the data as a waveform
+rows = 3
+cols = 3
+n = rows * cols
+fig, axes = plt.subplots(rows, cols, figsize=(16, 9))
+
+for i in range(n):
+  if i>=n:
+    break
+  r = i // cols
+  c = i % cols
+  ax = axes[r][c]
+  ax.plot(example_audio[i].numpy())
+  ax.set_yticks(np.arange(-1.2, 1.2, 0.2))
+  label = label_names[example_labels[i]]
+  ax.set_title(label)
+  ax.set_ylim([-1.1,1.1])
+
+plt.show()
+
