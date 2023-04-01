3635af9c3cc7e6465a8080f117ea55288719d06a
Elias Binder <ebinder@unibz.it>
2023-04-01 11:51:55 +0200

    Model can be trained by audio files located in resources/

--- src/ai/runner.py
+++ src/ai/runner.py
@@ -1,3 +1,5 @@
+# Documentation: https://www.tensorflow.org/tutorials/audio/simple_audio
+
 import os
 import pathlib
 
@@ -41,15 +43,20 @@ label_names = np.array(train_ds.class_names)
 print()
 print("label names:", label_names)
 
-# Drop extra axis from the labels
+# Drop extra axis from the audio, because audio is mono channel
 
 def squeeze(audio, labels):
-  audio = tf.squeeze(audio, axis=-1)
-  return audio, labels
+    audio = tf.squeeze(audio, axis=-1)
+    return audio, labels
 
 
 train_ds = train_ds.map(squeeze, tf.data.AUTOTUNE)
 val_ds = val_ds.map(squeeze, tf.data.AUTOTUNE)
+
+
+# Split data into half to use 1/2 for training and 1/2 for validation
+test_ds = val_ds.shard(num_shards=2, index=0)
+val_ds = val_ds.shard(num_shards=2, index=1)
 
 # Visualize the data as a waveform
 rows = 3
@@ -57,17 +64,24 @@ cols = 3
 n = rows * cols
 fig, axes = plt.subplots(rows, cols, figsize=(16, 9))
 
+
+for example_audio, example_labels in train_ds.take(1):
+    continue
+
 for i in range(n):
-  if i>=n:
-    break
-  r = i // cols
-  c = i % cols
-  ax = axes[r][c]
-  ax.plot(example_audio[i].numpy())
-  ax.set_yticks(np.arange(-1.2, 1.2, 0.2))
-  label = label_names[example_labels[i]]
-  ax.set_title(label)
-  ax.set_ylim([-1.1,1.1])
+    if i >= n:
+        break
+    r = i // cols
+    c = i % cols
+    ax = axes[r][c]
+    ax.plot(example_audio[i].numpy())
+    ax.set_yticks(np.arange(-1.2, 1.2, 0.2))
+    label = label_names[example_labels[i]]
+    ax.set_title(label)
+    ax.set_ylim([-1.1,1.1])
 
 plt.show()
+
+# Convert waveforms to spectrogram
+
 
